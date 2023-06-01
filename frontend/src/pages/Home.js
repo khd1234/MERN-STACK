9f3222d3e300b77ae3810c0394a48863fd6d4214
khadi <khadir.khan5@gmail.com>
2023-06-01 11:23:31 +0530

    WorkoutForm added

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 
 // components
 import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
   const [workouts, setWorkouts] = useState(null)
@@ -29,6 +30,7 @@ const Home = () => {
           />
         ))}
       </div>
+      <WorkoutForm />
     </div>
   )
 }
